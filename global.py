1a4232926afe66216ce224b86a78c04073281cba
Octavio Ragusa <onragusa93@gmail.com>
2023-11-10 14:36:07 -0300

    agrego def validacionLetra

--- global.py
+++ global.py
@@ -1,3 +1,8 @@
+def validacionLetra(letra):
+    #verifico si la letra es G,C,T,A y si es solamente una sola letra por cada ingreso
+    return letra.upper() in ['G','C','A','T'] and len(letra) == 1
+
+
 #creo la matriz
 dnaMatriz = []
 
@@ -6,7 +11,12 @@ for filas in range(6):
     fila = []
     for columnas in range(6):
         #variable para guardar la letra ingresada (con .upper() convierto a mayúsculas las letras)
-        letra = input("Ingrese la letra en la posición ({},{}): ".format(filas + 1,columnas + 1)).upper()
+        letra = input("Ingrese la letra en la posición ({},{}), solamente las letras G,C,A,T: ".format(filas + 1,columnas + 1)).upper()
+
+        #valido que la letra ingresada sea la pedida
+        while not validacionLetra(letra):
+            print("La letr ingresada no corresponde a lo pedido")
+            letra = input("Ingrese nuevamente una letra válida(G,C,A,T,), en la posición ({},{}): ".format(filas + 1,columnas + 1)).upper()
 
         #agrego la letra ingresada a la fila
         fila.append(letra)
