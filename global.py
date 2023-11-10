5eef33fc56b2327508b8d695b29b763e162a8978
Octavio Ragusa <onragusa93@gmail.com>
2023-11-10 15:32:24 -0300

    creo clase mutante, con el metodo isMutant y la verificacion de la horizontal

--- global.py
+++ global.py
@@ -1,3 +1,31 @@
+#creo clase mutante
+class mutante:
+    #recibe la matriz
+    def __init__(self,dnaMatriz):
+        self.dnaMatriz = dnaMatriz
+
+    #agrego el metodo isMutant peido por el ejercicio
+    def isMutant(self):
+        #guardo el len de la matriz
+        n = len(self.dnaMatriz)
+        #contador de secuencias iguales
+        contador = 0
+
+        #recorro matriz para buscar las letras iguales
+        for i in range(n):
+            for j in range(n - 3):
+                #verifico las horizontales
+                if self.secuenciaLetras(self.dnaMatriz[i][j:j+4]):
+                    contador += 1
+
+        #si el contador es mayor a uno devuelve True
+        return contador > 1
+
+    #verifico si hay 4 letras iguales seguidas        
+    def secuenciaLetras(self,secuencia):
+        return secuencia.count(secuencia[0]) == 4
+
+
 def validacionLetra(letra):
     #verifico si la letra es G,C,T,A y si es solamente una sola letra por cada ingreso
     return letra.upper() in ['G','C','A','T'] and len(letra) == 1
@@ -15,7 +43,7 @@ for filas in range(6):
 
         #valido que la letra ingresada sea la pedida
         while not validacionLetra(letra):
-            print("La letr ingresada no corresponde a lo pedido")
+            print("La letra ingresada no corresponde a lo pedido")
             letra = input("Ingrese nuevamente una letra válida(G,C,A,T,), en la posición ({},{}): ".format(filas + 1,columnas + 1)).upper()
 
         #agrego la letra ingresada a la fila
@@ -29,3 +57,14 @@ print("Matriz ingresada por el usuario")
 for row in dnaMatriz:
     print(" ".join(row))
 
+#instancio un objeto tipo mutante, le paso la matriz
+detector = mutante(dnaMatriz)
+
+#me fijo si la matriz ingresada corresponde a un mutante
+esMutante = detector.isMutant()
+
+#muestro si el adn ingresado corresponde o no a un mutante
+if esMutante:
+    print("El ADN ingresado corresponde a un mutante")
+else:
+    print("El ADN ingresado NO corresponde a un mutante")
