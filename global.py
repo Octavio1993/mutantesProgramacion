93d3f4ebccdea3a8fe4b759648e735b19611d4ff
Octavio Ragusa <onragusa93@gmail.com>
2023-11-12 12:40:56 -0300

    nuevas def para crear y mostrar matriz, y para validar la continuacion del programa

--- global.py
+++ global.py
@@ -13,33 +13,35 @@ class mutante:
         contador = 0
 
         #recorro matriz para buscar las letras iguales
-        """
         for i in range(n):
             for j in range(n - 3):
                 #verifico las horizontales
                 secHorizontal = self.dnaMatriz[i][j:j + 4]
                 if self.secuencia(secHorizontal):
                     contador += 1
+                    if contador > 1:
+                        return True
         
-        for i in range(n -3):
+        for i in range(n - 3):
             for j in range(n):
                 #verifico las verticales
                 if self.secuencia([self.dnaMatriz[k][j] for k in range(i, i + 4)]):
                     contador += 1
-        """
+        
 
         for i in range(n - 3):
             for j in range(n - 3):
+                
                 #verifico diagonal izquierda-derecha
                 secDiagonalIzqDer = [self.dnaMatriz[i + k][j + k] for k in range(4)]
                 if self.secuencia(secDiagonalIzqDer):
                     contador += 1
-                """
+                
                 #verifico diagonal derecha-izquierda
                 secDiagonalDerIzq = [self.dnaMatriz[i + k][j + 3 - k] for k in range(4)]
                 if self.secuencia(secDiagonalDerIzq):
                     contador += 1
-                """
+                
         if contador > 1:
             return True
 
@@ -48,45 +50,70 @@ class mutante:
         return len(set(secuencia)) == 1
 
 
+def ingresarMatriz():
+    #creo la matriz
+    dnaMatriz = []
+
+    #pido al usuario ingresar los valores dentro de la matriz
+    for filas in range(6):
+        fila = []
+        for columnas in range(6):
+            #variable para guardar la letra ingresada (con .upper() convierto a mayúsculas las letras)
+            letra = input("Ingrese la letra en la posición ({},{}), solamente las letras G,C,A,T: ".format(filas + 1,columnas + 1)).upper()
+
+            #valido que la letra ingresada sea la pedida
+            while not validacionLetra(letra):
+                print("La letra ingresada no corresponde a lo pedido")
+                letra = input("Ingrese nuevamente una letra válida(G,C,A,T,), en la posición ({},{}): ".format(filas + 1,columnas + 1)).upper()
+
+            #agrego la letra ingresada a la fila
+            fila.append(letra)
+        
+        #agrego la fila a la matriz
+        dnaMatriz.append(fila)
+
+    #devuelvo la matriz creada
+    return dnaMatriz
+
+def mostrarMatriz(dnaMatriz):
+    print("Matriz ingresada por el usuario")
+    for row in dnaMatriz:
+        print(" ".join(row))
+
 def validacionLetra(letra):
     #verifico si la letra es G,C,T,A y si es solamente una sola letra por cada ingreso
     return letra.upper() in ['G','C','A','T'] and len(letra) == 1
 
+def validacionRespuesta(respuesta):
+    resp = respuesta.lower()
+    while resp != "s" and resp != "n":
+        print("La respuesta debe ser solamente S o N")
+        respuesta = input("Ingrese nuevamente su respuesta")
+    if resp == "s":
+        return True
+    elif resp == "n":
+        return False
 
-#creo la matriz
-dnaMatriz = []
 
-#pido al usuario ingresar los valores dentro de la matriz
-for filas in range(6):
-    fila = []
-    for columnas in range(6):
-        #variable para guardar la letra ingresada (con .upper() convierto a mayúsculas las letras)
-        letra = input("Ingrese la letra en la posición ({},{}), solamente las letras G,C,A,T: ".format(filas + 1,columnas + 1)).upper()
-
-        #valido que la letra ingresada sea la pedida
-        while not validacionLetra(letra):
-            print("La letra ingresada no corresponde a lo pedido")
-            letra = input("Ingrese nuevamente una letra válida(G,C,A,T,), en la posición ({},{}): ".format(filas + 1,columnas + 1)).upper()
-
-        #agrego la letra ingresada a la fila
-        fila.append(letra)
+continuar = True
+while continuar:
+    #creo la matriz
+    dnaMatriz = ingresarMatriz()
     
-    #agrego la fila a la matriz
-    dnaMatriz.append(fila)
+    #muestro la matriz ingresada
+    mostrarMatriz(dnaMatriz)
 
-#muestro la matriz ingresada
-print("Matriz ingresada por el usuario")
-for row in dnaMatriz:
-    print(" ".join(row))
+    #instancio un objeto tipo mutante, le paso la matriz
+    detector = mutante(dnaMatriz)
 
-#instancio un objeto tipo mutante, le paso la matriz
-detector = mutante(dnaMatriz)
+    #me fijo si la matriz ingresada corresponde a un mutante
+    esMutante = detector.isMutant()
 
-#me fijo si la matriz ingresada corresponde a un mutante
-esMutante = detector.isMutant()
+    #muestro si el adn ingresado corresponde o no a un mutante
+    if esMutante:
+        print("El ADN ingresado corresponde a un mutante")
+    else:
+        print("El ADN ingresado NO corresponde a un mutante")
 
-#muestro si el adn ingresado corresponde o no a un mutante
-if esMutante:
-    print("El ADN ingresado corresponde a un mutante")
-else:
-    print("El ADN ingresado NO corresponde a un mutante")
+    respuesta = input("Desea continuar con la carga de ADN, para otra persona?(S/N)")
+    continuar = validacionRespuesta(respuesta)
